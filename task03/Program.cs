90290cb922f6c843bbe9026b34bae600359231e3
Rayenire <aeristarg@gmail.com>
2022-06-30 11:04:51 +0300

    Add task03

---
+++ task03/Program.cs
@@ -0,0 +1,7 @@
+﻿double x, y, x1, y1, len;
+x = Convert.ToDouble(Console.ReadLine());
+y = Convert.ToDouble(Console.ReadLine());
+x1 = Convert.ToDouble(Console.ReadLine());
+y1 = Convert.ToDouble(Console.ReadLine());
+len = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+Console.WriteLine(len);
